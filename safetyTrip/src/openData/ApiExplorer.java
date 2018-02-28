2f8b432c5ae3cf3b17c65a4222559fc71abcff05
joon <leejj90@naver.com>
2018-02-28 15:23:11 +0900

    OpenDataDTO.java insert

--- safetyTrip/src/openData/ApiExplorer.java
+++ safetyTrip/src/openData/ApiExplorer.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.*;
 import javax.xml.parsers.*;
 
@@ -15,18 +17,58 @@ import java.io.IOException;
 
 public class ApiExplorer {
     public static void main(String[] args) throws IOException {
-        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1262000/CountrySafetyService/getCountrySafetyList"); /*URL*/
+    	int no = 1;
+    	List<OpenDataDTO> oList = null;
+		
+    	int size = 1;
+    	while(size > 0) {
+    		try {
+    			oList = getCountrySafetyList(Integer.toString(no));
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+        	
+    		size = oList.size();
+    		
+    		for(OpenDataDTO oDTO: oList)
+    			System.out.println(oDTO.getWrtDt());
+    		// DB에 데이터 넣기
+    		
+    		String lastDt = oList.get(size-1).getWrtDt();
+    		String[] dts = lastDt.split("-");
+    		
+    		if(dts.length > 0) {
+	    		int year = Integer.parseInt(dts[0]);
+	    		
+	    		if(year < 2016) {
+	    			break;
+	    		} else {
+	    			no++;
+	    		}
+    		} else {
+    			break;
+    		}
+    	}
+        
+    }
+    
+    private static List<OpenDataDTO> getCountrySafetyList(String pageNo) throws Exception {
+    	List<OpenDataDTO> list = new ArrayList<>();
+    	
+    	StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1262000/CountrySafetyService/getCountrySafetyList"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=vDWEe0Bak75z6yuaW2hqlyT63bzPTV%2FmYuqosQF7tHGBdAydgsftGPol6dIRdj%2BgO2O7eayjm9bGCZ3EtZGIDA%3D%3D"); /*Service Key*/
 //        urlBuilder.append("&" + URLEncoder.encode("Content","UTF-8") + "=" + URLEncoder.encode("안전여행", "UTF-8")); /*내용*/
-//        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-//        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("100", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(pageNo, "UTF-8")); /*페이지 번호*/
 //        urlBuilder.append("&" + URLEncoder.encode("title","UTF-8") + "=" + URLEncoder.encode("소식", "UTF-8")); /*제목*/
+        
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
         System.out.println("Response code: " + conn.getResponseCode());
         BufferedReader rd;
+        
         if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         } else {
@@ -58,6 +100,7 @@ public class ApiExplorer {
         NodeList items = doc.getElementsByTagName("item");
 
         for(int i=0; i<items.getLength(); i++) {
+        	OpenDataDTO oDTO = new OpenDataDTO();
         	Node item = items.item(i);
         	Element ele = (Element)item;
 
@@ -68,37 +111,32 @@ public class ApiExplorer {
             NodeList titles = ele.getElementsByTagName("title");
             NodeList wrtDts = ele.getElementsByTagName("wrtDt");
             
-            String content = getTextContent(contents);
-            String contryEnName = getTextContent(countryEnNames);
-            String countryName = getTextContent(countryNames);
-            String id = getTextContent(ids);
-            String title = getTextContent(titles);
-            String wrtDt = getTextContent(wrtDts);
+            oDTO.setContent(getTextContent(contents));
+            oDTO.setCountryEnName(getTextContent(countryEnNames));
+            oDTO.setCountryName(getTextContent(countryNames));
+            oDTO.setId(getTextContent(ids));
+            oDTO.setTitle(getTextContent(titles));
+            oDTO.setWrtDt(getTextContent(wrtDts));
             
-            System.out.println();
-            System.out.println(content);
-            System.out.println(contryEnName);
-            System.out.println(countryName);
-            System.out.println(id);
-            System.out.println(title);
-            System.out.println(wrtDt);
-            System.out.println();
+            list.add(oDTO);
         }
         
+        return list;
     }
     
-    public static Document stringToDom(String xmlSource) 
+    private static Document stringToDom(String xmlSource) 
             throws SAXException, ParserConfigurationException, IOException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         return builder.parse(new InputSource(new StringReader(xmlSource)));
     }
     
-    public static String getTextContent(NodeList nl) {
+    private static String getTextContent(NodeList nl) {
     	Node n = nl.item(0);
-    	Element ele = (Element)n;
+    	Element ele = (Element) n;
     	
     	return ele.getTextContent();
     }
 
+    
 }
